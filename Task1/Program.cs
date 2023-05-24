556165dd5c1b5e136208569f857ff96dce3e8190
Sergey <neowaps1@mail.ru>
2023-05-24 21:02:32 +0300

    complete all task

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+System.Console.Write("Введите N: ");
+int n = int.Parse(System.Console.ReadLine());
+
+string SumNumbers(int numEnd) => numEnd == 1 ? numEnd.ToString() : numEnd + " " + SumNumbers(numEnd - 1);
+// {
+//     if (numEnd == 0) 
+// {
+//     return numEnd;
+// }
+// else
+// {
+//     System.Console.Write(numEnd + " ");
+//     return numEnd + SumNumbers(numEnd - 1);
+// }
+// }
+
+string sum = SumNumbers(n);
+System.Console.Write($"Все натуральные числа в промежутке от {n} до 1: {sum}");
